edd8b834ec2716875690dd1ea1ed13dd4440df8e
mterrian <matthew.terrian@gmail.com>
2017-07-13 09:35:13 -0400

    basic css and hover function

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
 console.log('Have fun!');
+
+//hover over seat and give it a new class
+$('.seat').hover(
+       function(){ $(this).addClass('hovering') },
+       function(){ $(this).removeClass('hovering') }
+);
