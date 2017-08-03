de40f58f46e3d8511eb6f75d29340017b54981ce
Stephanie McIlroy <stephanie.mcilroy@gmail.com>
2017-08-03 19:12:13 -0400

    styling

--- scripts/script.js
+++ scripts/script.js
@@ -39,12 +39,14 @@ $('form .btn').on('click', function() {
 	});
 	//sets data attribute of the selected anchor as user inputted form info
    	$('#' + selected).data('firstName', firstName);
+			$('#' + selected).data('lastName', lastName);
    	//declares the variable patron name to be whatever was input
-    var patronName = $('#' + selected).data('firstName');
+    var patronName1 = $('#' + selected).data('firstName');
+		var patronName2 = $('#' + selected).data('lastName');
     //finds the selected anchor and appends a div to its parent containing the selected seat and the name of patron
-    $('#' + selected).parent().append('<div class = "hoverdiv"> seat ' + selected + ' reserved by: ' + patronName + ' </div>');
+    $('#' + selected).parent().append('<div class = "hoverdiv"> Seat ' + selected + ' reserved by: ' + patronName1 + ' ' + patronName2 + ' </div>');
 // Add class/pargraph with patron name and seat reserved and append into the div
-		$('.thank-you').append('<div class="thankYou2"</div><p> Thank you ' + patronName + ' for reserving your seat: ' + selected + ' at Grand Circus!');
+		$('.thank-you').append('<div class="thankYou2"</div><p> Thank you ' + patronName1 + ' ' + patronName2 + ' for reserving your seat: ' + selected + ' at Grand Circus!');
 
 	});
 //sets default state of form to be hidden
